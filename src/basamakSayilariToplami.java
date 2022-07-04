7e5063093bbc2cc2e38c89181d30591ef97e773f
abdullahgurel <ag39@aof.anadolu.edu.tr>
2022-07-04 04:27:21 +0300

    basamakSayilariToplama

---
+++ src/basamakSayilariToplami.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class basamakSayilariToplami {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a,b=0,c=0;
+        System.out.print("Sayı Giriniz :");
+        a = input.nextInt();
+
+        while (a!=0) {
+
+
+            b = a%10;
+            a/=10;
+            System.out.println(b);
+            System.out.println(a);
+            c+=b;
+
+        }
+        System.out.println("c değeri : " + c);
+
+
+    }
+}
